9ac083c17e32e4b102802706d1c9a0420fb9cb11
znyadzi <znyadzi1@gmail.com>
2025-02-12 19:14:23 +0000

    Set up file upload functionality to sidebar for CSV data

--- src/components/app/app-sidebar.tsx
+++ src/components/app/app-sidebar.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, Plus } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { useRef } from "react";
 
 import {
   Sidebar,
@@ -12,10 +13,21 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarFooter,
 } from "@/components/ui/sidebar";
+import { Button } from "../ui/button";
 
 const AppSidebar = () => {
   const pathname = usePathname();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    
+    if (file) {
+      // Handle file upload
+    }
+  };
 
   return (
     <Sidebar>
@@ -35,6 +47,27 @@ const AppSidebar = () => {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <Button
+              variant="outline"
+              className="w-full rounded-full space-x-4"
+              onClick={() => fileInputRef.current?.click()}
+            >
+              <p>Upload CSV data</p>
+              <Plus className="w-6 h-6" />
+            </Button>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      <input
+        type="file"
+        accept="text/csv"
+        className="hidden"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
     </Sidebar>
   );
 };
